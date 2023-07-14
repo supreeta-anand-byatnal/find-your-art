2d769af0df182130f894ee0272ef1e3dd227f2a9
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-14 13:30:46 -0500

    add login form and buttons

--- frontend/src/components/LogIn/LogIn.js
+++ frontend/src/components/LogIn/LogIn.js
@@ -6,6 +6,20 @@ export default function LogIn() {
     <div className="login">
       <div className="side-image"/>
       <div className="login-panel">
+        <div className='login-form'>
+          <h1>Have an account? Log in.</h1>
+          <form>
+            <label for="email">Email</label>
+            <input></input>
+            <label for="password">Password</label>
+            <input></input>
+            <p>Forgot password?</p>
+            <button>Log in</button>
+            <button>Log in</button>
+            <button>Log in</button>
+            <button>Log in</button>
+          </form>
+        </div>
       </div>
     </div>
   )
