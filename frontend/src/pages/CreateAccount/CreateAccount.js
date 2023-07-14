dea05908b31c564668764dfe3cd9c2bbb3bd5ad7
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-14 18:12:32 -0500

    Create CreateAccount page

---
+++ frontend/src/pages/CreateAccount/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CreateAccount.css'
+
+export default function CreateAccount() {
+  return (
+    <div className="create">
+      <div className="create-side-image"/>
+      <div className="create-panel">
+      </div>
+    </div>
+  )
+}
