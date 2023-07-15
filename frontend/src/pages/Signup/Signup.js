41ef19eb48d2896d56180c88b47465097fc70fcb
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-14 21:57:06 -0500

    Refactor CreateAccount name to Signup

---
+++ frontend/src/pages/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Signup.css'
+
+export default function Signup() {
+  return (
+    <div className="signup">
+      <div className="signup-side-image"/>
+      <div className="signup-panel">
+      </div>
+    </div>
+  )
+}
