d16b2d6e42289a32ce33cb00af394581dfe19279
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-15 17:56:41 -0500

    Create Role page

---
+++ frontend/src/pages/Role/Role.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Role.css'
+
+export default function Role() {
+  return (
+    <div className="role">
+      <div className="role-side-image" />
+      <div className="role-panel">
+      </div>
+    </div>
+  )
+}
