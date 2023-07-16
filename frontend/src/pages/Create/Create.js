44ce676f7e62b05bb7c423044a4522c7ef52701f
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-15 23:34:01 -0500

    Refactor Signup to Create page for reuse in create account process

---
+++ frontend/src/pages/Create/Create.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Create.css";
+import SignupForm from "../../components/SignupForm/SignupForm";
+import RoleForm from '../../components/RoleForm/RoleForm'
+
+export default function Create() {
+  return (
+    <div className="create">
+      <div className="create-side-image" />
+      <div className="create-panel"></div>
+    </div>
+  );
+}
