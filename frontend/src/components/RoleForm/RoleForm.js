8fc95561d9cc7dc8d8ea8c8a751558213e3f4290
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-15 22:18:27 -0500

    Reformat RoleForm code with Prettier

--- frontend/src/components/RoleForm/RoleForm.js
+++ frontend/src/components/RoleForm/RoleForm.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import './RoleForm.css'
+import React from "react";
+import "./RoleForm.css";
 
 export default function RoleForm() {
   return (
     <div id="role-form">
       <h1 id="role-title">Welcome! What is your role?</h1>
-      <form> 
-        <button className="role-button" type="button" value="teacherEducator">Teacher/Educator</button>
-        <button className="role-button" type="button" value="artOrganization">Art organization</button>
-        <button className="role-button" value="teachingArtist">Teaching artist</button>
+      <form>
+        <button className="role-button" type="button" value="teacherEducator">
+          Teacher/Educator
+        </button>
+        <button className="role-button" type="button" value="artOrganization">
+          Art organization
+        </button>
+        <button className="role-button" value="teachingArtist">
+          Teaching artist
+        </button>
       </form>
     </div>
-  )
+  );
 }
