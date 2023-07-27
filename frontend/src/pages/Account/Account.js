6b0c3b93e67d782405a01488ca78ebec6b2df7ec
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-27 17:52:49 -0500

    Add logo and update CSS to to fix cutoffs

--- frontend/src/pages/Account/Account.js
+++ frontend/src/pages/Account/Account.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Account.css';
+import logo from '../../assets/fya-logo.svg'
 
 export default function Account({ children }) {
   return (
-    <div className="account">
-      <div className="account-side-image" />
-      <div className="account-form">{children}</div>
+    <div id="account">
+      <div id="account-side-image" />
+      <div id="account-side-form">
+        <div id="account-form-container"> 
+          <img src={logo} alt="fya logo" />
+          <div id="account-form-content">
+            {children}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
