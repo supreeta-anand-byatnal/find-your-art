f78038d92737a0f7d431a55033c8346d31896d5a
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-07-31 01:05:47 -0500

    Add parameter to login request

--- frontend/src/modules/users.js
+++ frontend/src/modules/users.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/login'
 
-const login = () => {
-  const response = axios.post(baseUrl)
+const login = userData => {
+  const response = axios.post(baseUrl, userData)
   return response.then(response => response.data)
 }
 
