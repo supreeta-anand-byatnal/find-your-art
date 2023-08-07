6414c5c314dd165abd3a927b77d5c702d132763f
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-08-06 23:56:34 -0500

    Add HTTP request for signup

--- frontend/src/modules/users.js
+++ frontend/src/modules/users.js
@@ -7,6 +7,11 @@ const login = userData => {
   return response.then(response => response.data)
 }
 
-const moduleExport = { login }
+const signUp = userData => {
+  const response = axios.post(baseUrl + '/auth/signup', userData)
+  return response.then(response => response.data)
+}
+
+const moduleExport = { login, signUp }
 
 export default moduleExport
