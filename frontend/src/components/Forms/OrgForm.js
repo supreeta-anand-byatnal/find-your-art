7d62a4b2553795b501fc0d5db5185510672e9d95
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-08-08 23:20:23 -0500

    Add submit functionality for organization role

--- frontend/src/components/Forms/OrgForm.js
+++ frontend/src/components/Forms/OrgForm.js
@@ -5,39 +5,52 @@ import LinksField from './LinksField/LinksField';
 import TagField from './TagField/TagField';
 import DoubleField from './DoubleField/DoubleField';
 
-export default function OrgForm() {
+export default function OrgForm({ signUpData, handleSignUpInput, handleSignUpRequest }) {
+  function handleClick(e) {
+    handleSignUpRequest();
+    e.preventDefault();
+  }
+
   return (
     <div className="form">
       <h1>Let's set up your Arts Organization profile!</h1>
       <form>
         <SingleField
           inputTitle="Organization name"
-          inputID="org-name"
+          inputID="signUpOrganization"
           inputType="text"
           inputPH="Organization name"
+          signUpData={signUpData}
+          handleSignUpInput={handleSignUpInput}
         />
         <SingleField
           inputTitle="Website"
-          inputID="org-website"
+          inputID="signUpWebsite"
           inputType="text"
           inputPH="www.organization.com"
+          signUpData={signUpData}
+          handleSignUpInput={handleSignUpInput}
         />
-        <LinksField inputTitle="Social Media Link(s)" inputPH="www.yoursocialmedia.com"/>
+        <LinksField inputID="signUpLinks" inputTitle="Social Media Link(s)" inputPH="www.yoursocialmedia.com"  handleSignUpInput={handleSignUpInput} signUpData={signUpData}/>
         <TagField
-          inputID="org-discipline"
+          inputID="signUpDisciplines"
           inputTitle="Art Disciplines(s)"
           inputPH="Cinema, painting, etc."
+          signUpData={signUpData}
+          handleSignUpInput={handleSignUpInput}
         />
         <DoubleField
-          leftInputID="org-city"
-          rightInputID="org-zip"
+          leftInputID="signUpCity"
+          rightInputID="signUpZip"
           leftInputTitle="City"
           rightInputTitle="Zip Code"
           inputType="text"
           leftInputPH="Atlanta, Seattle, etc."
           rightInputPH="000000"
+          signUpData={signUpData}
+          handleSignUpInput={handleSignUpInput}
         />
-        <button className="form-submit">Next</button>
+        <button className="form-submit" onClick={e => handleClick(e)}>Next</button>
       </form>
     </div>
   );
