f3d37e264c964bd49f3465a4fb7a6c18ce0449ba
yhu4592 <hu.yongkang@protonmail.com>
2023-08-17 00:21:26 -0500

    Serve build of frontend

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(morgan('tiny'))
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 
 // For testing only
 app.get('/', (req, res) => {
