ef8d248e324b043399d81be86ea6979803b4b3bf
linh <linh.ha.nguyen20@gmail.com>
2023-09-11 19:59:30 -0400

    merge files

---
+++ backend/controllers/fileUploader.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const multer = require('multer');
+const User = require('./models/user');
+const Event = require('./models/event');
+
+const app = express();
+
+// Set up Multer to handle image uploads
+const storage = multer.memoryStorage(); // Store the image in memory as a Buffer
+const upload = multer({ storage: storage });
+
+// Route to handle profile picture upload
+app.post('/upload-profile-picture', upload.single('profilePicture'), async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+
+    if (!user) {
+      return res.status(404).send('User not found.');
+    }
+
+    // Update the profilePicture field with the uploaded image data
+    user.profilePicture = {
+      data: req.file.buffer,
+      contentType: req.file.mimetype,
+    };
+
+    await user.save();
+    res.status(200).send('Profile picture uploaded successfully!');
+  } catch (err) {
+    console.error('Error uploading profile picture:', err);
+    res.status(500).send('Error uploading profile picture.');
+  }
+});
+
+app.get('/profile-picture/:userId', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId);
+  
+      if (!user || !user.profilePicture.data) {
+        return res.status(404).send('Profile picture not found.');
+      }
+  
+      res.setHeader('Content-Type', user.profilePicture.contentType);
+      res.send(user.profilePicture.data);
+    } catch (err) {
+      console.error('Error retrieving profile picture:', err);
+      res.status(500).send('Error retrieving profile picture.');
+    }
+  });
+
+  // Route to handle image upload for events
+app.post('/upload-event-images/:eventId', upload.array('eventImages', 3), async (req, res) => {
+    try {
+      const event = await Event.findById(req.params.eventId);
+  
+      if (!event) {
+        return res.status(404).send('Event not found.');
+      }
+  
+      // Update the images field with the uploaded image data
+      req.files.forEach((file) => {
+        event.images.push({
+          data: file.buffer,
+          contentType: file.mimetype,
+        });
+      });
+  
+      await event.save();
+      res.status(200).send('Event images uploaded successfully!');
+    } catch (err) {
+      console.error('Error uploading event images:', err);
+      res.status(500).send('Error uploading event images.');
+    }
+  });
+
+  app.get('/event-image/:eventId/:imageIndex', async (req, res) => {
+    try {
+      const event = await Event.findById(req.params.eventId);
+  
+      if (!event || !event.images[req.params.imageIndex]) {
+        return res.status(404).send('Event image not found.');
+      }
+  
+      const image = event.images[req.params.imageIndex];
+  
+      res.setHeader('Content-Type', image.contentType);
+      res.send(image.data);
+    } catch (err) {
+      console.error('Error retrieving event image:', err);
+      res.status(500).send('Error retrieving event image.');
+    }
+  });
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,77 @@
-const app = require('./app')
+const express = require('express');
+const morgan = require('morgan');
+const authRoutes = require('./controllers/auth');
+const userRoutes = require('./controllers/user');
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+// For testing only
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+
+// DATA CONNECTION
+const mongoose = require('mongoose');
+const User = require('./models/user.js')
+const Educator = require('./models/educator.js');
+const Partner = require('./models/partner.js');
+const TeachingArtist = require('./models/teachingArtist.js');
+const Event = require('./models/event.js');
+const Image = require('./models/image.js');
+
+async function main() {
+    const uri =
+  "mongodb+srv://rfg9:findYourArt23@cluster0.4xcfy2s.mongodb.net/FindYourArt?retryWrites=true&w=majority";
+
+    try {
+        // Connect to the MongoDB cluster
+        await mongoose.connect(uri);
+        console.log("Connected");
+
+        /*
+        //insert an Educator
+        const e1 = new Educator({
+            profilePicture: Buffer,
+            firstName: 'Eric',
+            lastName: 'Smith',
+            pronouns: 'he/him',
+            email: 'esmith@gvhs.org',
+            passwordHash: 'password123',
+            schoolName: 'Green Valley High School',
+            subjects: ['History'],
+            groups: ['Tenth Grade'],
+            city: 'Kenmore',
+            zipcode: '98028',
+            verified: false,
+        });
+
+        e1.save()
+        .then((savedUser) => {
+          console.log('User created:', savedUser);
+        })
+        .catch((err) => {
+          console.error('Error creating user:', err);
+        });
+        */
+
+    } catch (e) {
+        console.log("Could not connect");
+    } finally {
+        //await mongoose.disconnect();
+    }
+}
+
+main().catch(err => console.log(err));
+// ----------------------------------
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
+});
