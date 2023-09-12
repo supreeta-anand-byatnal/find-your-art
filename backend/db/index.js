2f0d007f2f30aec17e7d97eef8517d26aadeb089
linh <linh.ha.nguyen20@gmail.com>
2023-09-11 23:06:41 -0400

    refactor db connection script

---
+++ backend/db/index.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+// const User = require('./db/models/user.js')
+// const Educator = require('./db/models/educator.js');
+// const Partner = require('./db/models/partner.js');
+// const TeachingArtist = require('./db/models/teachingArtist.js');
+// const Event = require('./db/models/event.js');
+// const Image = require('./db/models/image.js');
+require('dotenv').config();
+const DB_URI = process.env.DB_URI;
+
+const connectDB = async () => {
+    try {
+        // Connect to the MongoDB cluster
+        await mongoose.connect(DB_URI);
+        console.log("Connected");
+
+        /*
+        //insert an Educator
+        const e1 = new Educator({
+            profilePicture: Buffer,
+            firstName: 'Eric',
+            lastName: 'Smith',
+            pronouns: 'he/him',
+            email: 'esmith@gvhs.org',
+            passwordHash: 'password123',
+            schoolName: 'Green Valley High School',
+            subjects: ['History'],
+            groups: ['Tenth Grade'],
+            city: 'Kenmore',
+            zipcode: '98028',
+            verified: false,
+        });
+
+        e1.save()
+        .then((savedUser) => {
+          console.log('User created:', savedUser);
+        })
+        .catch((err) => {
+          console.error('Error creating user:', err);
+        });
+        */
+
+    } catch (e) {
+        console.log("Could not connect");
+    } finally {
+        //await mongoose.disconnect();
+    }
+}
+
+connectDB().catch(err => console.log(err));
+
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const connectDB = require('./db');
 const authRoutes = require('./controllers/auth');
 const userRoutes = require('./controllers/user');
 
-require('dotenv').config()
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+
+connectDB();
 
 // For testing only
 app.get('/', (req, res) => {
@@ -17,61 +22,7 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 
-// DATA CONNECTION
-const mongoose = require('mongoose');
-const User = require('./models/user.js')
-const Educator = require('./models/educator.js');
-const Partner = require('./models/partner.js');
-const TeachingArtist = require('./models/teachingArtist.js');
-const Event = require('./models/event.js');
-const Image = require('./models/image.js');
 
-async function main() {
-    const uri =
-  "mongodb+srv://rfg9:findYourArt23@cluster0.4xcfy2s.mongodb.net/FindYourArt?retryWrites=true&w=majority";
-
-    try {
-        // Connect to the MongoDB cluster
-        await mongoose.connect(uri);
-        console.log("Connected");
-
-        /*
-        //insert an Educator
-        const e1 = new Educator({
-            profilePicture: Buffer,
-            firstName: 'Eric',
-            lastName: 'Smith',
-            pronouns: 'he/him',
-            email: 'esmith@gvhs.org',
-            passwordHash: 'password123',
-            schoolName: 'Green Valley High School',
-            subjects: ['History'],
-            groups: ['Tenth Grade'],
-            city: 'Kenmore',
-            zipcode: '98028',
-            verified: false,
-        });
-
-        e1.save()
-        .then((savedUser) => {
-          console.log('User created:', savedUser);
-        })
-        .catch((err) => {
-          console.error('Error creating user:', err);
-        });
-        */
-
-    } catch (e) {
-        console.log("Could not connect");
-    } finally {
-        //await mongoose.disconnect();
-    }
-}
-
-main().catch(err => console.log(err));
-// ----------------------------------
-
-const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
